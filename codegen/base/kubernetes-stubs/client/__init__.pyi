e5b571ec5f6b8ab8fecbc56b8df6e32bbf2d9ebc
Elephant Liu <lexdene@gmail.com>
2024-05-07 15:21:25 +0800

    explicitly export by all instead of import as

--- codegen/base/kubernetes-stubs/client/__init__.pyi
+++ codegen/base/kubernetes-stubs/client/__init__.pyi
@@ -1,7 +1,15 @@
-from kubernetes.client.api_client import ApiClient as ApiClient
-from kubernetes.client.configuration import Configuration as Configuration
-from kubernetes.client.exceptions import ApiException as ApiException
-from kubernetes.client.exceptions import ApiKeyError as ApiKeyError
-from kubernetes.client.exceptions import ApiTypeError as ApiTypeError
-from kubernetes.client.exceptions import ApiValueError as ApiValueError
-from kubernetes.client.exceptions import OpenApiException as OpenApiException
+from kubernetes.client.api_client import ApiClient
+from kubernetes.client.configuration import Configuration
+from kubernetes.client.exceptions import (ApiException, ApiKeyError,
+                                          ApiTypeError, ApiValueError,
+                                          OpenApiException)
+
+__all__ = [
+    "ApiClient",
+    "Configuration",
+    "ApiException",
+    "ApiKeyError",
+    "ApiTypeError",
+    "ApiValueError",
+    "OpenApiException",
+]
